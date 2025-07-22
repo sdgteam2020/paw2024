2c5e3f7cb61650a2814f80f2cfbedaee598ba284
tomertech-code <arbaztomer0@gmail.com>
2025-07-22 14:56:33 +0530

    Commit

---
+++ swas.BAL/Interfaces/IRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using swas.BAL.DTO;
+using swas.DAL.Models;
+
+namespace swas.BAL.Interfaces
+{
+    public interface IRemainder
+    {
+        Task<int> AddRemainder(int psmid, int fromUnitId, int toUnitId, string remarks, string userDetails);
+        Task<List<RemainderDisplayDto>> ProjectRemainderMovHistory(int? ProjectId);
+
+        Task<int> GetProjectById(string? ProjName);
+
+        Task<List<RemainderDisplayDto>> GetAllAsync();
+        //Task<DTOpR>
+
+    }
+}   
